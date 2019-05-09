3fbb4fca3613a0ca7e4874ad4adc7f6fcb227caf
haemin <jhm9063@naver.com>
2019-05-09 20:20:23 +0900

    xxxx

---
+++ src/main/java/org/hm/post/PostMain.java
@@ -0,0 +1,21 @@
+package org.hm.post;
+
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PostMain {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+				"post-context.xml");
+		PostService postService = context.getBean("postService",
+				PostService.class);
+
+		postService.addPost();
+		postService.listPosts();
+		postService.likePost();
+		context.close();
+	}
+
+}
